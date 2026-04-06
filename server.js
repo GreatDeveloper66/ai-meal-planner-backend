87ce93435a5457567ba4c49e2d2ce95c87af9433
GreatDeveloper66 <ashaffe@gmail.com>
2026-04-06 14:39:50 -0500

    Add server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Meal Planner API is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
